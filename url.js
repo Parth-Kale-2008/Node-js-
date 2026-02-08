93d8f64788d0a4b758f86b09c0a2a93e51cb0c6c
Parth-Kale-2008 <parthkale416@gmail.com>
2026-02-08 13:07:01 +0530

    It’s a Mongoose schema for a URL shortener.  shortId → the short code (unique)  redirectUrl → the original long URL  visitHistory → logs each click with timestamp

--- url.js
+++ url.js
@@ -1,6 +1,18 @@
-const express = require('express');
-const {handleGenerateNewShortURL} = require('../controller/url');
-const router = express.Router();
-router.post = ("/",handleGenerateNewShortURL);
-module.exports = router;
+const mongoose = require('mongoose');
+const urlSchema = new mongoose.Schema({
+    shortId:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    redirectUrl:{
+        type:String,
+        required:true,
+    },
+    visitHistory:[{timeStamp:{type:Number}}],
+},
+{timeStamp:true}
+);
 
+const URL = mongoose.model('url',urlSchema);
+module.exports = URL;
